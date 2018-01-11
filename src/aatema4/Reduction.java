e16688917babde8f40fc936b0e2fb4934bfa3942
edwmrkg <35065545+edwmrkg@users.noreply.github.com>
2018-01-11 22:00:10 +0200

    it wasn't done

--- src/aatema4/Reduction.java
+++ src/aatema4/Reduction.java
@@ -9,12 +9,11 @@ public class Reduction {
 	private int noOfVertices;
 	private int noOfEdges;
 	private int noOfColors;
-	private TreeMap<Integer,Integer> edges = new TreeMap<Integer, Integer>();
-	
+	private TreeMap<Integer, Integer> edges = new TreeMap<Integer, Integer>();
+
 	/**
-	 * Reads from file and store the data of the graph.
-	 * Number of vertices, of edges and of colors.
-	 * Also builds a list of edges.
+	 * Reads from file and store the data of the graph. Number of vertices, of edges
+	 * and of colors. Also builds a list of edges.
 	 * @param file - a String representing the name of the file.
 	 */
 	public void readFile(String file) {
@@ -24,7 +23,7 @@ public class Reduction {
 			noOfVertices = Integer.parseInt(line[0]);
 			noOfEdges = Integer.parseInt(line[1]);
 			noOfColors = Integer.parseInt(line[2]);
-			
+
 			for (int i = 0; i < noOfEdges; i++) {
 				line = in.readLine().split(" ");
 				edges.put(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
@@ -33,55 +32,59 @@ public class Reduction {
 		} catch (Exception e) {
 			System.out.println("File not found!");
 			return;
-		} 
+		}
 	}
-		
-	
+
 	public void solve(String file) {
 		try {
 			PrintWriter out = new PrintWriter(file);
 			for (int i = 0; i < noOfVertices; i++) {
-				out.print("(");
+				if (noOfColors > 1)
+					out.print("(");
 				for (int j = 0; j < noOfColors; j++) {
 					int var = i * noOfColors + j;
 					out.print("x" + var);
-					if (var * noOfColors != noOfColors - 1) 
+					if (j < noOfColors - 1)
 						out.print("V");
 				}
-				out.print(")");
-			}
-			out.print("^");
-			for (int i = 0; i < noOfVertices; i++) {
-				for (int j = 0 ; j < noOfColors - 1; j++) {
-					for (int k = j + 1; k < noOfColors; k++) {
-						out.print("(~x");
-						out.print(i * noOfColors + j);
-						out.print("V~x");
-						out.print(i * noOfColors + k);
-						out.print(")");
-					}
-					out.print("^");
-				}
-			}
-			int counter = 0;
-			for (Entry<Integer, Integer> edge : edges.entrySet()) {
-				int vertex1 = edge.getKey();
-				int vertex2 = edge.getValue();
-				for (int i = 0; i < noOfColors; i++) {
-					++counter;
-					out.print("(~x");
-					out.print(noOfColors * vertex1 + i);
-					out.print("V~x");
-					out.print(noOfColors * vertex2 + i);
+				if (noOfColors > 1)
 					out.print(")");
-					if (counter < noOfColors * noOfEdges) 
-						out.print("^");
+				if (i < noOfVertices - 1)
+					out.print("^");
+			}
+			if (noOfColors > 0) {
+				for (int i = 0; i < noOfVertices; i++) {
+					for (int j = 0; j < noOfColors - 1; j++) {
+						for (int k = j + 1; k < noOfColors; k++) {
+							out.print("(~x");
+							out.print(i * noOfColors + j);
+							out.print("V~x");
+							out.print(i * noOfColors + k);
+							out.print(")^");
+						}
+					}
+				}
+				int counter = 0;
+				for (Entry<Integer, Integer> edge : edges.entrySet()) {
+					int vertex1 = edge.getKey();
+					int vertex2 = edge.getValue();
+					for (int i = 0; i < noOfColors; i++) {
+						++counter;
+						out.print("(~x");
+						out.print(noOfColors * vertex1 + i);
+						out.print("V~x");
+						out.print(noOfColors * vertex2 + i);
+						out.print(")");
+						if (counter < noOfColors * noOfEdges)
+							out.print("^");
+					}
 				}
 			}
+			out.println();
 			out.close();
 		} catch (Exception e) {
 			System.out.println("Cannot create output file.");
 		}
-		
-	}		
+
+	}
 }
